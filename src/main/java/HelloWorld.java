83d19572129840bf3ec34bdbc37dfaac5adb2410
actuallyfro <actuallyfro@github.com>
2021-05-17 21:40:32 -0500

    So that built ... haven't run, too scared. lul

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,12 @@
 
 import static org.asciidoctor.Asciidoctor.Factory.create;
 import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Attributes;
+import org.asciidoctor.Options;
+
+import javax.xml.stream.events.Attribute;
 import java.io.File; //https://www.geeksforgeeks.org/file-class-in-java/
+import java.util.HashMap; //https://www.w3schools.com/java/java_hashmap.asp
 
 class HelloWorld {
     public static void main(String[] args) {
@@ -20,7 +25,17 @@ class HelloWorld {
         //String output = asciidoctor.convertFile(new File("baeldung.adoc"), new HashMap<String, Object>());
 
         //https://raw.githubusercontent.com/asciidoctor/asciidoctor-pdf/main/examples/basic-example.adoc
-        String output = asciidoctor.convertFile(new File("basic-example.adoc"), new HashMap<String, Object>());
+        //String output = asciidoctor.convertFile(new File("basic-example.adoc"), new HashMap<String, Object>());
+
+        //https://docs.asciidoctor.org/asciidoctorj/latest/conversion-examples DEPRICATED!
+        //Map<String, Object> attributes = Attributes.builder().backend("docbook").icons("font").asMap();
+        //Map<String, Object> options = Options.builder().inPlace(true).attributes(attributes).asMap();
+        Attributes attributes = Attributes.builder().backend("docbook").icons("font").build(); //COMPLETELY GUESSING!!!
+        Options options = Options.builder().inPlace(true).attributes(attributes).build(); //COMPLETELY GUESSING!!!
+
+        //String output = asciidoctor.convertFile(new File("basic-example.adoc"), new HashMap<String, Object>());
+        //String output = asciidoctor.convertFile(new File("basic-example.adoc"), options); //Attempt from the Docs pages
+        String output = asciidoctor.convertFile(new File("basic-example.adoc"), options); //COMPLETELY GUESSING!!!
 
     }
 
