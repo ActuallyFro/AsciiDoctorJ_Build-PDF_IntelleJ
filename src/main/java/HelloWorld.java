a696c991d9f2457d92650c7b3b7ab9c7d17fb054
actuallyfro <actuallyfro@github.com>
2021-05-19 23:42:33 -0500

    {source/jar} Compiling to directory-defined, backend-defined Jar file to generate outputs

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,23 +2,52 @@ import static org.asciidoctor.Asciidoctor.Factory.create;
 import org.asciidoctor.Asciidoctor;
 import org.asciidoctor.Attributes;
 import org.asciidoctor.Options;
+import org.asciidoctor.jruby.AsciiDocDirectoryWalker;
+
 
 import java.io.File;
 import java.text.DecimalFormat;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class HelloWorld {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         long stopTime;
-        System.out.println("Generating an example PDF from an .adoc File!");
+
+        String directory=".";
+
+        if(args.length > 0){
+            directory = args[0];
+            Path path = Paths.get(directory);
+            if (!Files.isDirectory(path)){
+                path = Paths.get(".");
+            }
+            directory = path.toString();
+        }
+
+        String backendStr = "pdf";
+        if(args.length > 1) {
+            backendStr = args[1];
+        }
+        System.out.println("Generating "+backendStr+"(s) from an .adoc File(s)!");
 
         Asciidoctor asciidoctor = create();
 
         //File Settings/Options
         Attributes attributes = Attributes.builder().icons("font").build(); //No idea what this does...
-        Options options = Options.builder().inPlace(true).backend("pdf").attributes(attributes).build();
+        Options options = Options.builder().inPlace(true).backend(backendStr).attributes(attributes).build(); //TRY-CATCH!?!?!
 
-        asciidoctor.convertFile(new File("basic-example.adoc"), options);
+//        String[] result = asciidoctor.convertDirectory(new AsciiDocDirectoryWalker(directory), options);
+        //https://www.codota.com/code/java/methods/org.asciidoctor.Asciidoctor/convertDirectory
+//        public String[] convertDirectory(String baseDir) {
+//            final AsciiDocDirectoryWalker directoryWalker = new AsciiDocDirectoryWalker(baseDir);
+//            return asciidoctor.convertDirectory(directoryWalker, options);
+//        }
+
+        asciidoctor.convertDirectory(new AsciiDocDirectoryWalker(directory), options); //ALL files within a directory
+        //asciidoctor.convertFile(new File("basic-example.adoc"), options); //single file
 
         stopTime = System.currentTimeMillis();
         long timeElapsed = stopTime - startTime;
